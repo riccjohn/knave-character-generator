9c363f736b1e852f0b7eb812b077d78f6ba016c0
riccjohn <riccjohn@gmail.com>
2021-09-22 19:38:08 -0500

    Render <Abilities /> in storybook with context

--- src/components/Abilities.stories.tsx
+++ src/components/Abilities.stories.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { Meta } from "@storybook/react";
 import { Abilities } from "./";
+import CharacterContext from "context/CharacterContext";
+import Character from "character_generation/Character";
 
 export default {
   component: Abilities,
   title: "Components/Abilities",
 } as Meta;
 
-export const Primary: React.VFC<{}> = () => <Abilities />;
+const character = new Character();
+character.generate();
+
+const context = {
+  character,
+};
+
+export const Primary: React.VFC<{}> = () => {
+  return (
+    <CharacterContext.Provider value={context}>
+      <Abilities />
+    </CharacterContext.Provider>
+  );
+};
