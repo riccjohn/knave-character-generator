77c48eabfd3111c1374b2a8ad39ddcb5bdfb7faf
riccjohn <riccjohn@gmail.com>
2021-10-29 14:22:45 -0500

    7-Items Use Shell in Abilities.story

--- src/components/Abilities/Abilities.stories.tsx
+++ src/components/Abilities/Abilities.stories.tsx
@@ -1,25 +1,21 @@
-import React from "react";
-import { Meta } from "@storybook/react";
-import Abilities from ".";
-import CharacterContext from "context/CharacterContext";
-import Character from "character_generation/Character";
+import React from "react"
+import { Meta } from "@storybook/react"
+import Abilities from "."
+import Character from "character_generation/Character"
+import { Shell } from "components"
 
 export default {
   component: Abilities,
   title: "Components/Abilities",
-} as Meta;
+} as Meta
 
-const character = new Character();
-character.generate();
-
-const context = {
-  character,
-};
+const character = new Character()
+character.generate()
 
 export const Primary: React.VFC<{}> = () => {
   return (
-    <CharacterContext.Provider value={context}>
+    <Shell character={character}>
       <Abilities />
-    </CharacterContext.Provider>
-  );
-};
+    </Shell>
+  )
+}
